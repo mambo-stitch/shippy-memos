6a39dd9fa2f39bd41b4b66c9ff6c45a4660c6b3c
mambo-stitch <mambo-stitch0s@icloud.com>
2024-07-02 04:33:44 +0000

    update depploy script. also lets make this commit message annoyingly long, how does the ui handle really long names in commit messages and... yeah, you know? it's frustrating when you have to be able to handle suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuper long names in the commit message

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -4,12 +4,14 @@ echo "Pulling the latest Docker image..."
 docker pull mambostitch/mambo-stitch:${CIRCLE_SHA1}
 
 echo "Stopping existing Docker containers..."
-docker stop $(docker ps -q --filter ancestor=mambostitch/mambo-stitch)
+# Using specific tag to stop containers
+docker stop $(docker ps -q --filter "ancestor=mambostitch/mambo-stitch:${CIRCLE_SHA1}")
 
 echo "Removing stopped containers..."
-docker rm $(docker ps -a -q --filter ancestor=mambostitch/mambo-stitch)
+# Using specific tag to remove containers
+docker rm $(docker ps -a -q --filter "ancestor=mambostitch/mambo-stitch:${CIRCLE_SHA1}")
 
 echo "Starting new Docker container..."
 docker run -d -p 80:80 --name mambo-stitch mambostitch/mambo-stitch:${CIRCLE_SHA1}
 
-echo "Deployment complete."
+echo "Shippy complete!"
