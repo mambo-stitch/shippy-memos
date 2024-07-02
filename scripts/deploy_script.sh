5940f04744f81f04a3ce87d86fcb3cbf37bc9bf6
mambo-stitch <mambo-stitch0s@icloud.com>
2024-07-02 04:17:45 +0000

    trying to make the build work

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Stop all running containers (simplified approach for demonstration purposes)
-docker stop $(docker ps -aq)
-
-# Remove all stopped containers
-docker rm $(docker ps -aq)
-
-# Pull the latest image from Docker Hub
+echo "Pulling the latest Docker image..."
 docker pull mambostitch/mambo-stitch:${CIRCLE_SHA1}
 
-# Start the new container
-docker run -d -p 80:80 mambostitch/mambo-stitch:${CIRCLE_SHA1}
+echo "Stopping existing Docker containers..."
+docker stop $(docker ps -q --filter ancestor=mambostitch/mambo-stitch)
+
+echo "Removing stopped containers..."
+docker rm $(docker ps -a -q --filter ancestor=mambostitch/mambo-stitch)
+
+echo "Starting new Docker container..."
+docker run -d -p 80:80 --name mambo-stitch mambostitch/mambo-stitch:${CIRCLE_SHA1}
+
+echo "Deployment complete."
