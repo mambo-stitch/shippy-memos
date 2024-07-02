d5e45437f8fe33e1b83814fa34c36da6ad17a49d
mambo-stitch <mambo-stitch0s@icloud.com>
2024-07-02 04:51:01 +0000

    update script

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -3,24 +3,18 @@
 echo "Pulling the latest Docker image..."
 docker pull mambostitch/mambo-stitch:${CIRCLE_SHA1}
 
-echo "Stopping existing Docker containers..."
-# Using specific tag to stop containers
-docker stop $(docker ps -q --filter "ancestor=mambostitch/mambo-stitch:${CIRCLE_SHA1}")
-
-echo "Removing stopped containers..."
-# Using specific tag to remove containers
-docker rm $(docker ps -a -q --filter "ancestor=mambostitch/mambo-stitch:${CIRCLE_SHA1}")
-
-# Check for running containers and stop them if any
+echo "Checking for existing containers to stop..."
 containers_to_stop=$(docker ps -q --filter "ancestor=mambostitch/mambo-stitch:${CIRCLE_SHA1}")
 if [ ! -z "$containers_to_stop" ]; then
+    echo "Stopping containers: $containers_to_stop"
     docker stop $containers_to_stop
+    echo "Removing containers: $containers_to_stop"
     docker rm $containers_to_stop
 else
-    echo "No containers to stop."
+    echo "No existing containers to stop."
 fi
 
-# Start the new container
+echo "Starting new Docker container..."
 docker run -d -p 80:80 --name mambo-stitch mambostitch/mambo-stitch:${CIRCLE_SHA1}
 
-echo "Shippy complete!"
+echo "Deployment complete."
